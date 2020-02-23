c88e6283b5c1c693f262194b36b06169f367b7cb
Michael McLoughlin <mmcloughlin@gmail.com>
2020-02-23 14:37:13 -0800

    pkg/wrap: use realtime priority

--- pkg/wrap/pri.go
+++ pkg/wrap/pri.go
@@ -14,6 +14,7 @@ import (
 type Prioritize struct {
 	command.Base
 
+	rt   int
 	nice int
 }
 
@@ -34,7 +35,8 @@ func (*Prioritize) Usage() string {
 }
 
 func (cmd *Prioritize) SetFlags(f *flag.FlagSet) {
-	f.IntVar(&cmd.nice, "nice", -20, "nice value")
+	f.IntVar(&cmd.rt, "rt", 99, "if non-zero, attempt to set realtime priority")
+	f.IntVar(&cmd.nice, "nice", -20, "nice value, used as a fallback if realtime fails")
 }
 
 func (cmd *Prioritize) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
@@ -44,8 +46,20 @@ func (cmd *Prioritize) Execute(_ context.Context, f *flag.FlagSet, _ ...interfac
 	// Apply process priority.
 	runtime.LockOSThread()
 
-	if err := proc.SetPriority(0, cmd.nice); err != nil {
-		return cmd.Error(err)
+	nice := true
+	if cmd.rt != 0 {
+		err := proc.SetScheduler(0, proc.SCHED_RR, &proc.SchedParam{Priority: cmd.rt})
+		if err != nil {
+			cmd.Log.Printf("failed to set realtime priority")
+		} else {
+			nice = false
+		}
+	}
+
+	if nice {
+		if err := proc.SetPriority(0, cmd.nice); err != nil {
+			return cmd.Error(err)
+		}
 	}
 
 	// Execute the sub-process.
