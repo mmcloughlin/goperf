0c80f52320bbda7885fc21d04546ef4816094fca
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-05 01:48:29 -0800

    fn/perfdataupload: implement upload
    First upload: https://perf.golang.org/search?q=upload:20200305.4

--- fn/perfdataupload/fn.go
+++ fn/perfdataupload/fn.go
@@ -2,48 +2,82 @@ package perfdataupload
 
 import (
 	"context"
+	"io"
 	"log"
-	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/build/buildenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	perfstorage "golang.org/x/perf/storage"
+	oauth2api "google.golang.org/api/oauth2/v2"
 )
 
 // GCSEvent is the payload of a GCS event.
 type GCSEvent struct {
-	Kind                    string                 `json:"kind"`
-	ID                      string                 `json:"id"`
-	SelfLink                string                 `json:"selfLink"`
-	Name                    string                 `json:"name"`
-	Bucket                  string                 `json:"bucket"`
-	Generation              string                 `json:"generation"`
-	Metageneration          string                 `json:"metageneration"`
-	ContentType             string                 `json:"contentType"`
-	TimeCreated             time.Time              `json:"timeCreated"`
-	Updated                 time.Time              `json:"updated"`
-	TemporaryHold           bool                   `json:"temporaryHold"`
-	EventBasedHold          bool                   `json:"eventBasedHold"`
-	RetentionExpirationTime time.Time              `json:"retentionExpirationTime"`
-	StorageClass            string                 `json:"storageClass"`
-	TimeStorageClassUpdated time.Time              `json:"timeStorageClassUpdated"`
-	Size                    string                 `json:"size"`
-	MD5Hash                 string                 `json:"md5Hash"`
-	MediaLink               string                 `json:"mediaLink"`
-	ContentEncoding         string                 `json:"contentEncoding"`
-	ContentDisposition      string                 `json:"contentDisposition"`
-	CacheControl            string                 `json:"cacheControl"`
-	Metadata                map[string]interface{} `json:"metadata"`
-	CRC32C                  string                 `json:"crc32c"`
-	ComponentCount          int                    `json:"componentCount"`
-	Etag                    string                 `json:"etag"`
-	CustomerEncryption      struct {
-		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
-		KeySha256           string `json:"keySha256"`
-	}
-	KMSKeyName    string `json:"kmsKeyName"`
-	ResourceState string `json:"resourceState"`
+	Bucket string `json:"bucket"`
+	Name   string `json:"name"`
 }
 
 // Handle GCS event.
 func Handle(ctx context.Context, e GCSEvent) error {
-	log.Printf("Bucket: %v\n", e.Bucket)
-	log.Printf("File: %v\n", e.Name)
+	log.Printf("bucket: %v\n", e.Bucket)
+	log.Printf("file: %v\n", e.Name)
+
+	// GCS client.
+	gcs, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Perf storage client.
+	perfstore, err := NewPerfStorageClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Open the GCS object.
+	obj := gcs.Bucket(e.Bucket).Object(e.Name)
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	// Start an upload.
+	u := perfstore.NewUpload(ctx)
+	w, err := u.CreateFile(e.Name)
+	if err != nil {
+		u.Abort()
+		return err
+	}
+
+	// Transfer from bucket to perf store.
+	if _, err := io.Copy(w, r); err != nil {
+		u.Abort()
+		return err
+	}
+
+	// Finalize.
+	status, err := u.Commit()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("upload id: %v\n", status.UploadID)
+	log.Printf("view url: %v\n", status.ViewURL)
+
 	return nil
+}
+
+func NewPerfStorageClient(ctx context.Context) (*perfstorage.Client, error) {
+	creds, err := google.FindDefaultCredentials(ctx, oauth2api.UserinfoEmailScope)
+	if err != nil {
+		return nil, err
+	}
+
+	return &perfstorage.Client{
+		BaseURL:    buildenv.Production.PerfDataURL,
+		HTTPClient: oauth2.NewClient(ctx, creds.TokenSource),
+	}, nil
 }
--- fn/perfdataupload/go.mod
+++ fn/perfdataupload/go.mod
@@ -3,3 +3,11 @@ module github.com/mmcloughlin/cb/fn/perfdataupload
 go 1.13
 
 replace github.com/mmcloughlin/cb => ../..
+
+require (
+	cloud.google.com/go/storage v1.6.0
+	golang.org/x/build v0.0.0-20200304223525-ef9e68dfbdfe
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/perf v0.0.0-20200225203053-adf48cbc4550
+	google.golang.org/api v0.18.0
+)
