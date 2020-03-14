fd94da5db9832956155f44c7613c8d79f9370519
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-14 12:04:48 -0700

    internal/test: TempDir

--- internal/test/test.go
+++ internal/test/test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"flag"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -11,5 +13,22 @@ func RequiresNetwork(t *testing.T) {
 	t.Helper()
 	if !*network {
 		t.Skip("requires network")
+	}
+}
+
+// TempDir creates a temp directory. Returns the path to the directory and a
+// cleanup function.
+func TempDir(t *testing.T) (string, func()) {
+	t.Helper()
+
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return dir, func() {
+		if err := os.RemoveAll(dir); err != nil {
+			t.Fatal(err)
+		}
 	}
 }
