059ddb20c821a22e562d09dfc890683af0ca3ec5
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-18 01:11:02 -0700

    app/obj: new package (#20)
    Abstraction for simple object storage.

---
+++ app/obj/obj.go
@@ -0,0 +1,66 @@
+// Package obj provides abstractions for simple object storage.
+package obj
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+// KeyNotFoundError indicates that a key was not found in an object store.
+type KeyNotFoundError struct {
+	Key Key
+}
+
+// Error returns an error message, as required by the error interface.
+func (e KeyNotFoundError) Error() string {
+	k := e.Key
+	return fmt.Sprintf("key (%s,%s) not found", k.Type(), k.ID())
+}
+
+// Key identifies an object.
+type Key interface {
+	Type() string
+	ID() string
+}
+
+type key struct {
+	t  string
+	id string
+}
+
+// K builds a key with the given type and ID.
+func K(t, id string) Key {
+	return key{
+		t:  t,
+		id: id,
+	}
+}
+
+func (k key) Type() string { return k.t }
+func (k key) ID() string   { return k.id }
+
+// Object is a serializable object with an associated Key. All objects should be
+// JSON serializable. Other serializaiton methods may be required by certain
+// storage systems.
+type Object interface {
+	Key
+}
+
+// Store is a method of storing objects by key.
+type Store interface {
+	Get(context.Context, Key, Object) error
+	Set(context.Context, Object) error
+}
+
+// Encode the object to w.
+func Encode(w io.Writer, v Object) error {
+	return msgpack.NewEncoder(w).UseJSONTag(true).Encode(v)
+}
+
+// Decode an object from r.
+func Decode(r io.Reader, v Object) error {
+	return msgpack.NewDecoder(r).UseJSONTag(true).Decode(v)
+}
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/nwaples/rardecode v1.0.0 // indirect
 	github.com/pierrec/lz4 v2.4.1+incompatible // indirect
 	github.com/shirou/gopsutil v2.20.1+incompatible
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/build v0.0.0-20200207163221-ab7b028cb90c
 	golang.org/x/mod v0.1.0
