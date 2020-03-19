e2b2e881fc5e1b6ac16e6d26aac5eb89c18f0bf8
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-18 20:23:42 -0700

    app/obj: firestore object store

---
+++ app/obj/firestore.go
@@ -0,0 +1,34 @@
+package obj
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
+type firestorestore struct {
+	client *firestore.Client
+}
+
+// NewFirestore builds an object store backed by Google Firestore.
+func NewFirestore(c *firestore.Client) Store {
+	return &firestorestore{
+		client: c,
+	}
+}
+
+func (s *firestorestore) Get(ctx context.Context, k Key, v Object) error {
+	// Fetch from firestore.
+	snap, err := s.client.Collection(k.Type()).Doc(k.ID()).Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal.
+	return snap.DataTo(v)
+}
+
+func (s *firestorestore) Set(ctx context.Context, v Object) error {
+	_, err := s.client.Collection(v.Type()).Doc(v.ID()).Set(ctx, v)
+	return err
+}
