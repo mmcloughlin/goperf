cb39309e5743fbb61dac9c02b7eab33e8ee2e9a6
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-19 00:21:52 -0700

    app/model: module, package, benchmark

--- app/model/zmodels.go
+++ app/model/zmodels.go
@@ -19,3 +19,32 @@ type Commit struct {
 
 func (c *Commit) Type() string { return "commits" }
 func (c *Commit) ID() string   { return c.SHA }
+
+type Module struct {
+	UUID    string `firestore:"uuid" json:"uuid"`
+	Path    string `firestore:"path" json:"path"`
+	Version string `firestore:"version" json:"version"`
+}
+
+func (m *Module) Type() string { return "modules" }
+func (m *Module) ID() string   { return m.UUID }
+
+type Package struct {
+	UUID         string `firestore:"uuid" json:"uuid"`
+	ModuleUUID   string `firestore:"module_uuid" json:"module_uuid"`
+	RelativePath string `firestore:"relative_path" json:"relative_path"`
+}
+
+func (p *Package) Type() string { return "packages" }
+func (p *Package) ID() string   { return p.UUID }
+
+type Benchmark struct {
+	UUID        string            `firestore:"uuid" json:"uuid"`
+	PackageUUID string            `firestore:"package_uuid" json:"package_uuid"`
+	Name        string            `firestore:"name" json:"name"`
+	Unit        string            `firestore:"unit" json:"unit"`
+	Parameters  map[string]string `firestore:"parameters" json:"parameters"`
+}
+
+func (b *Benchmark) Type() string { return "benchmarks" }
+func (b *Benchmark) ID() string   { return b.UUID }
