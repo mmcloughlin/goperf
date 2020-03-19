53631ef65a613a4918919fc3cc35134b6e374405
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-18 22:32:43 -0700

    app/repo: add missing json tags

--- app/repo/store.go
+++ app/repo/store.go
@@ -45,11 +45,11 @@ type fscommit struct {
 	Parents        []string  `firestore:"parents" json:"parents"`
 	AuthorName     string    `firestore:"author_name" json:"author_name"`
 	AuthorEmail    string    `firestore:"author_email" json:"author_email"`
-	AuthorTime     time.Time `firestore:"author_time"`
-	CommitterName  string    `firestore:"committer_name"`
-	CommitterEmail string    `firestore:"committer_email"`
-	CommitTime     time.Time `firestore:"commit_time"`
-	Message        string    `firestore:"message"`
+	AuthorTime     time.Time `firestore:"author_time" json:"author_time"`
+	CommitterName  string    `firestore:"committer_name" json:"committer_name"`
+	CommitterEmail string    `firestore:"committer_email" json:"committer_email"`
+	CommitTime     time.Time `firestore:"commit_time" json:"commit_time"`
+	Message        string    `firestore:"message" json:"message"`
 }
 
 func tofscommit(c *Commit) *fscommit {
