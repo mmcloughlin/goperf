2e45170a526f863cbb5a3cdbc2c720ee9c135cd1
Michael McLoughlin <mmcloughlin@gmail.com>
2020-03-25 00:10:34 -0700

    app/db: init

--- app/db/db.go
+++ app/db/db.go
@@ -1,3 +1,29 @@
 package db
 
+import (
+	"database/sql"
+
+	"github.com/mmcloughlin/cb/app/db/internal/db"
+)
+
 //go:generate sqlc generate
+
+type DB struct {
+	db *sql.DB
+	q  *db.Queries
+}
+
+func Open(conn string) (*DB, error) {
+	d, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{
+		db: d,
+		q:  db.New(d),
+	}, nil
+}
+
+func (d *DB) Close() error {
+	return d.db.Close()
+}
