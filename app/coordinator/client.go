9f7794075c9334f1362f88c515f7c25589a99c45
Michael McLoughlin <mmcloughlin@gmail.com>
2020-04-11 13:57:59 -0700

    app/coordinator: refactor client

--- app/coordinator/client.go
+++ app/coordinator/client.go
@@ -25,103 +25,63 @@ func NewClient(c *http.Client, url, worker string) *Client {
 }
 
 func (c *Client) Jobs(ctx context.Context) (*JobsResponse, error) {
-	// Build request.
-	u := c.url + "/workers/" + c.worker + "/jobs"
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, u, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	// Execute the request.
-	res, err := c.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	if err := httputil.ExpectStatus(res.StatusCode, http.StatusOK, http.StatusCreated); err != nil {
-		return nil, err
-	}
-
-	// Decode JSON response.
 	payload := &JobsResponse{}
-	if err := httputil.DecodeJSON(res.Body, payload); err != nil {
+	if err := c.request(ctx, params{
+		Method:         http.MethodPost,
+		Path:           "/workers/" + c.worker + "/jobs",
+		AcceptStatuses: []int{http.StatusOK, http.StatusCreated},
+		Payload:        payload,
+	}); err != nil {
 		return nil, err
 	}
-
 	return payload, nil
 }
 
 func (c *Client) Start(ctx context.Context, id uuid.UUID) error {
-	// Build request.
-	u := c.url + "/workers/" + c.worker + "/jobs/" + id.String() + "/start"
-	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u, nil)
-	if err != nil {
-		return err
-	}
-
-	// Execute the request.
-	res, err := c.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if err := httputil.ExpectStatus(res.StatusCode, http.StatusNoContent); err != nil {
-		return err
-	}
-
-	return nil
+	return c.request(ctx, params{
+		Method:         http.MethodPut,
+		Path:           "/workers/" + c.worker + "/jobs/" + id.String() + "/start",
+		AcceptStatuses: []int{http.StatusNoContent},
+	})
 }
 
 func (c *Client) UploadResult(ctx context.Context, id uuid.UUID, r io.Reader) error {
-	// Build request.
-	u := c.url + "/workers/" + c.worker + "/jobs/" + id.String() + "/result"
-	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u, r)
-	if err != nil {
-		return err
-	}
-
-	// Execute the request.
-	res, err := c.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if err := httputil.ExpectStatus(res.StatusCode, http.StatusNoContent); err != nil {
-		return err
-	}
-
-	return nil
+	return c.request(ctx, params{
+		Method:         http.MethodPut,
+		Path:           "/workers/" + c.worker + "/jobs/" + id.String() + "/result",
+		Body:           r,
+		AcceptStatuses: []int{http.StatusNoContent},
+	})
 }
 
 func (c *Client) Fail(ctx context.Context, id uuid.UUID) error {
-	// Build request.
-	u := c.url + "/workers/" + c.worker + "/jobs/" + id.String() + "/fail"
-	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u, nil)
-	if err != nil {
-		return err
-	}
-
-	// Execute the request.
-	res, err := c.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if err := httputil.ExpectStatus(res.StatusCode, http.StatusNoContent); err != nil {
-		return err
-	}
-
-	return nil
+	return c.request(ctx, params{
+		Method:         http.MethodPut,
+		Path:           "/workers/" + c.worker + "/jobs/" + id.String() + "/fail",
+		AcceptStatuses: []int{http.StatusNoContent},
+	})
 }
 
 func (c *Client) Halt(ctx context.Context, id uuid.UUID) error {
+	return c.request(ctx, params{
+		Method:         http.MethodPut,
+		Path:           "/workers/" + c.worker + "/jobs/" + id.String() + "/halt",
+		AcceptStatuses: []int{http.StatusNoContent},
+	})
+}
+
+type params struct {
+	Method         string
+	Path           string
+	Body           io.Reader
+	AcceptStatuses []int
+	Payload        interface{}
+}
+
+func (c *Client) request(ctx context.Context, p params) error {
 	// Build request.
-	u := c.url + "/workers/" + c.worker + "/jobs/" + id.String() + "/halt"
-	req, err := http.NewRequestWithContext(ctx, http.MethodPut, u, nil)
+	url := c.url + p.Path
+	req, err := http.NewRequestWithContext(ctx, p.Method, url, p.Body)
 	if err != nil {
 		return err
 	}
@@ -133,8 +93,15 @@ func (c *Client) Halt(ctx context.Context, id uuid.UUID) error {
 	}
 	defer res.Body.Close()
 
-	if err := httputil.ExpectStatus(res.StatusCode, http.StatusNoContent); err != nil {
+	if err := httputil.ExpectStatus(res.StatusCode, p.AcceptStatuses...); err != nil {
 		return err
+	}
+
+	// Decode JSON response.
+	if p.Payload != nil {
+		if err := httputil.DecodeJSON(res.Body, p.Payload); err != nil {
+			return err
+		}
 	}
 
 	return nil
