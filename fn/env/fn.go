5f77d49272147a7b412b33ae1dff6f0bbebab783
Michael McLoughlin <mmcloughlin@gmail.com>
2020-04-12 18:30:17 -0700

    fn/env: use service logger

--- fn/env/fn.go
+++ fn/env/fn.go
@@ -1,18 +1,32 @@
 package env
 
 import (
-	"log"
+	"context"
 	"net/http"
 	"os"
 
 	"github.com/mmcloughlin/cb/app/httputil"
+	"github.com/mmcloughlin/cb/app/service"
+	"go.uber.org/zap"
 )
+
+// Initialization.
+var logger *zap.Logger
+
+func initialize(ctx context.Context, l *zap.Logger) error {
+	logger = l
+	return nil
+}
+
+func init() {
+	service.Initialize(initialize)
+}
 
 // Handle HTTP trigger.
 func Handle(w http.ResponseWriter, r *http.Request) {
 	// Log environment.
 	for _, e := range os.Environ() {
-		log.Println(e)
+		logger.Info(e)
 	}
 
 	// Report ok.
--- fn/env/go.mod
+++ fn/env/go.mod
@@ -4,4 +4,7 @@ go 1.13
 
 replace github.com/mmcloughlin/cb => ../..
 
-require github.com/mmcloughlin/cb v0.0.0-00010101000000-000000000000
+require (
+	github.com/mmcloughlin/cb v0.0.0-00010101000000-000000000000
+	go.uber.org/zap v1.14.1
+)
